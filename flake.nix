6cfa085a5d3573440489f8c659858c5776a60b1a
Emp. Justine <1224205+empjustine@users.noreply.github.com>
2023-11-30 23:38:34 -0300

    flakeref=github:NixOS/nixpkgs/67be70a859530f6f7c358568eaa6ab0d84b36b01

--- flake.nix
+++ flake.nix
@@ -1,39 +1,37 @@
 {
   inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+
   outputs = {
     self,
     nixpkgs-stable,
   }: {
-    apps.x86_64-linux.git-gui = {
-      type = "app";
-      program = "${nixpkgs-stable.legacyPackages.x86_64-linux.gitFull}/libexec/git-core/git-gui";
-    };
-    apps.x86_64-linux.gitk = {
-      type = "app";
-      program = "${nixpkgs-stable.legacyPackages.x86_64-linux.gitFull}/bin/gitk";
-    };
-
-    packages.x86_64-linux.git-tk = nixpkgs-stable.legacyPackages.x86_64-linux.buildEnv {
-      name = "git-tk";
-      paths = with nixpkgs-stable.legacyPackages.x86_64-linux; [
-        git-doc
-      ];
-      pathsToLink = ["/share/man" "/share/doc" "/share/info" "/bin" "/etc"];
-      extraOutputsToInstall = ["man" "doc" "info"];
-      postBuild = ''
-        ln -s ${nixpkgs-stable.legacyPackages.x86_64-linux.gitFull}/libexec/git-core/git-gui $out/bin/git-gui
-        ln -s ${nixpkgs-stable.legacyPackages.x86_64-linux.gitFull}/bin/gitk $out/bin/gitk
-
-        # info
-        if [ -x $out/bin/install-info -a -w $out/share/info ]; then
-          shopt -s nullglob
-          for i in $out/share/info/*.info $out/share/info/*.info.gz; do
-              $out/bin/install-info $i $out/share/info/dir
-          done
-        fi
-      '';
+    apps.x86_64-linux = let
+      pkgs = nixpkgs-stable.legacyPackages.x86_64-linux;
+    in {
+      git-gui = {
+        type = "app";
+        program = "${pkgs.gitFull}/libexec/git-core/git-gui";
+      };
+      gitk = {
+        type = "app";
+        program = "${pkgs.gitFull}/bin/gitk";
+      };
     };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.git-tk;
+    packages.x86_64-linux = let
+      pkgs = nixpkgs-stable.legacyPackages.x86_64-linux;
+    in {
+      git-tk = pkgs.buildEnv {
+        name = "git-tk";
+        paths = [];
+        postBuild = ''
+          mkdir -p $out/bin
+          ln -s ${pkgs.gitFull}/libexec/git-core/git-gui $out/bin/git-gui
+          ln -s ${pkgs.gitFull}/bin/gitk $out/bin/gitk
+        '';
+      };
+
+      default = self.packages.x86_64-linux.git-tk;
+    };
   };
 }
